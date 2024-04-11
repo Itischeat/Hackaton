b6db1255b223bd88ec12ad459e83ed1385dc88cd
Aleksandr <jamp.ivonin@bk.ru>
2024-04-12 01:58:50 +0300

    add db

--- db.js
+++ db.js
@@ -1,4 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    
+    user: 'postgres',
+    password: 'ItischeaT1',
+    host: 'localhost',
+    port: 5432,
+    database: 'hackaton'
 })
+
+module.exports = pool
